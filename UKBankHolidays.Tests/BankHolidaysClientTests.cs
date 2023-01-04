9345a5f1965b356614a47147c37d33ed3a79cdbf
Joe Sumpner <joe.sumpner@simpson-associates.co.uk>
2023-01-04 11:49:45 +0400

    Added basic unit tests

---
+++ UKBankHolidays.Tests/BankHolidaysClientTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Moq.Protected;
+using UKBankHolidays.Services;
+using UKBankHolidays.Models;
+using System.Text.Json;
+
+namespace UKBankHolidays.Tests
+{
+    public class BankHolidaysClientTests
+    {
+        private static Mock<HttpMessageHandler> GetMockedMessageHandler(BankHolidaysResponse testContent)
+        {
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+            mockMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(JsonSerializer.Serialize(testContent))
+                });
+
+            return mockMessageHandler;
+        }
+
+        [Fact]
+        public async Task GetBankHolidaysAsync_ReturnsBankHolidays()
+        {
+            BankHolidaysResponse testContent = DummyResponse.Valid;
+
+            var mockMessageHandler = GetMockedMessageHandler(testContent);
+
+            var underTest = new BankHolidaysClient(new HttpClient(mockMessageHandler.Object));
+
+            // Act
+            var result = await underTest.GetBankHolidaysAsync();
+
+            // Assert
+            Assert.NotEmpty(result.EnglandAndWales.Events);
+            Assert.NotEmpty(result.Scotland.Events);
+            Assert.NotEmpty(result.NorthernIreland.Events);
+        }
+
+        [Fact]
+        public async Task GetBankHolidaysInYearAsync_ReturnsBankHolidays_ForEnglandAndWales()
+        {
+            BankHolidaysResponse testContent = DummyResponse.Valid;
+            int testYear = 2020;
+
+            var mockMessageHandler = GetMockedMessageHandler(testContent);
+
+            var underTest = new BankHolidaysClient(new HttpClient(mockMessageHandler.Object));
+
+            // Act
+            var result = await underTest.GetBankHolidaysInYearAsync(testYear);
+
+            // Assert
+            var eventsInYear = testContent.EnglandAndWales.Events.Where(x => x.Date.Year == testYear).ToList();
+            Assert.NotEmpty(eventsInYear);
+
+            Assert.NotEmpty(result.EnglandAndWales.Events);
+
+            Assert.All(result.EnglandAndWales.Events, x => Assert.Equal(testYear, x.Date.Year));
+        }
+
+        [Fact]
+        public async Task GetBankHolidaysFromDateAsync_ReturnsBankHolidays_ForEnglandAndWales()
+        {
+            BankHolidaysResponse testContent = DummyResponse.Valid;
+            DateTime testDate = new(2020, 3, 3);
+
+            var mockMessageHandler = GetMockedMessageHandler(testContent);
+
+            var underTest = new BankHolidaysClient(new HttpClient(mockMessageHandler.Object));
+
+            // Act
+            var result = await underTest.GetBankHolidaysFromDateAsync(testDate);
+
+            // Assert
+            var eventsFromDate = testContent.EnglandAndWales.Events.Where(x => x.Date >= testDate).ToList();
+            Assert.NotEmpty(eventsFromDate);
+
+            Assert.NotEmpty(result.EnglandAndWales.Events);
+
+            Assert.All(result.EnglandAndWales.Events, x => Assert.True(x.Date >= testDate));
+        }
+    }
+}
---
+++ UKBankHolidays.Tests/DummyResponse.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKBankHolidays.Models;
+
+namespace UKBankHolidays.Tests
+{
+    internal static class DummyResponse
+    {
+        /// <summary>
+        /// Returns a dummy valid response from the bank holidays API.
+        /// </summary>
+        public static BankHolidaysResponse Valid = new()
+        {
+            EnglandAndWales = new Division()
+            { 
+                Name = "england-and-wales",
+                Events = new List<BankHoliday>()
+                {
+                    new BankHoliday()
+                    {
+                        Title = "EW Bank Holiday 1",
+                        Date = new DateTime(2020, 1, 1)
+                    },
+                    new BankHoliday()
+                    {
+                        Title = "EW Bank Holiday 2",
+                        Date = new DateTime(2020, 2, 2)
+                    },
+                    new BankHoliday()
+                    {
+                        Title = "EW Bank Holiday 3",
+                        Date = new DateTime(2020, 3, 3)
+                    },
+                    new BankHoliday()
+                    {
+                        Title = "EW Bank Holiday 4",
+                        Date = new DateTime(2021, 10, 10)
+                    }
+                }
+            },
+
+            Scotland = new Division()
+            {
+                Name = "scotland",
+                Events = new List<BankHoliday>()
+                {
+                    new BankHoliday()
+                    {
+                        Title = "S Bank Holiday 1",
+                        Date = new DateTime(2021, 4, 4)
+                    },
+                    new BankHoliday()
+                    {
+                        Title = "S Bank Holiday 2",
+                        Date = new DateTime(2021, 5, 5)
+                    },
+                    new BankHoliday()
+                    {
+                        Title = "S Bank Holiday 3",
+                        Date = new DateTime(2021, 6, 6)
+                    }
+                }
+            },
+
+            NorthernIreland = new Division()
+            {
+                Name = "northern-ireland",
+                Events = new List<BankHoliday>()
+                {
+                    new BankHoliday()
+                    {
+                        Title = "NI Bank Holiday 1",
+                        Date = new DateTime(2022, 7, 7)
+                    },
+                    new BankHoliday()
+                    {
+                        Title = "NI Bank Holiday 2",
+                        Date = new DateTime(2022, 8, 8)
+                    }
+                }
+            }
+        };
+
+    }
+}
